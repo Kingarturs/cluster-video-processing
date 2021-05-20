db9e64024a09e3d78506b5cf0a4d087969ad7d36
Luis Arturo García González <lgarcia103@alumnos.uaq.mx>
2021-05-19 20:13:05 -0500

    Added local ip in order to test with other devices

--- ProcessingServer.js
+++ ProcessingServer.js
@@ -2,4 +2,4 @@ const fs = require('fs');
 const io = require('socket.io-client');
 const ss = require('socket.io-stream');
 
-const socket = io.connect('http://localhost:8000/processing');
+const socket = io.connect('http://192.168.0.5:8000/processing');
